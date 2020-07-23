85e67e7eae463c3fe799f77c5a763e7458644c91
Anson Nickel <anson.nickel@gmail.com>
2020-07-23 20:36:21 +0000

    Done.

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,34 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    og_item = find_item_by_name_in_collection(coupon[:item], cart)
+    if og_item != nil && og_item[:count] >= coupon[:num] 
+      og_item[:count] -= coupon[:num] 
+      cart << {item: coupon[:item] + " W/COUPON", price: coupon[:cost] / coupon[:num], clearance: og_item[:clearance], count: coupon[:num]}
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  cart.map do |item|
+    if item[:clearance]
+      item[:price] = (item[:price]*0.8).round(2)
+    end
+    item
+  end
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  
+  ultimate_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  
+  bill = 0
+  
+  ultimate_cart.each {|item| bill += item[:count]*item[:price]}
+  
+  bill > 100 ? bill *= 0.9 : bill
+  bill.round(2)
 end
